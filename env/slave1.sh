1b9b8f5ae49a799bdf49700281c18c1ea99f891e
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-04-27 11:04:15 +0700

    update host

--- env/slave1.sh
+++ env/slave1.sh
@@ -2,5 +2,5 @@
 
 export TIMEZONE="Asia/Jakarta"
 export PORT_SLAVE1="3302"
-export HOST_SLAVE1="mariadb-slave1"
+export HOST_SLAVE1="mariadb_mariadb-slave1"
 export SLAVE1_ROOT_PASSWORD="SLAVE1_ROOT_PASSWORD_SET"
