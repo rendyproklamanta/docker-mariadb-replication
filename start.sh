3061d55ee366b6dd1fc3134855097566a2bd404b
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-06-14 15:25:15 +0000

    update

--- start.sh
+++ start.sh
@@ -10,27 +10,27 @@ docker stack rm mariadb
 
 # Deploy master
 mkdir -p data/master
-docker stack deploy --compose-file docker-compose.master.yaml mariadb
+docker stack deploy --compose-file docker-compose.master.yaml --detach=false mariadb
 cd scripts
 chmod +x deploy.master.sh && ./deploy.master.sh
 cd ../
 
 # Deploy slave1
 mkdir -p data/slave1
-docker stack deploy --compose-file docker-compose.slave1.yaml mariadb
+docker stack deploy --compose-file docker-compose.slave1.yaml --detach=false mariadb
 cd scripts
 chmod +x deploy.slave1.sh && ./deploy.slave1.sh
 cd ../
 
 # Deploy MaxScale
-docker stack deploy --compose-file docker-compose.maxscale.yaml mariadb
+docker stack deploy --compose-file docker-compose.maxscale.yaml --detach=false mariadb
 
 # Deploy PMA
-docker stack deploy --compose-file docker-compose.pma.yaml mariadb
+docker stack deploy --compose-file docker-compose.pma.yaml --detach=false mariadb
 
 # Deploy backup
 mkdir -p data/backup
-docker stack deploy --compose-file docker-compose.backup.yaml mariadb
+docker stack deploy --compose-file docker-compose.backup.yaml --detach=false mariadb
 
 # Set permission
 chmod -R 777 data
