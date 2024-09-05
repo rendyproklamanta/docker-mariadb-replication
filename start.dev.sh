fedb38509852818dbcbd13a894240bcce5a6deb8
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-09-05 22:53:34 +0700

    update

--- start.dev.sh
+++ start.dev.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 
-# Stopping all services
-docker stack rm mariadb
+# Define color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color (reset to default)
+
+# load env file into the script's environment.
+source ./env/global.sh
+source ./env/master.sh
+source ./env/slave1.sh
 
 # Init swarm
 docker swarm init
 docker network create --driver overlay mysql-network
 
-## Deploy replication
-cd replication
+# Stopping all services
+docker stack rm mariadb
 
 # Deploy master
-cd master
-chmod +x init.sh && ./init.sh
-cd ../
+echo "${RED}*** Deploy container master ***${NC}"
+cd ./replication/master
+mkdir -p data
+chmod -R 777 data
+chmod +x init/init-sql.sh && init/init-sql.sh
+docker stack deploy --compose-file docker-compose.yaml --detach=false mariadb
+echo "[*] Waiting 30s for master container to be up and running..."
+sleep 30
+
+cd ../../
 
 # Deploy slave1
-cd slave1
-chmod +x init.sh && ./init.sh
-cd ../
+echo "${RED}*** Deploy container slave1 ***${NC}"
+cd ./replication/slave1
+mkdir -p data
+chmod -R 777 data
+chmod +x init/init-sql.sh && init/init-sql.sh
+docker stack deploy --compose-file docker-compose.yaml --detach=false mariadb
+echo "[*] Waiting 30s for slave container to be up and running..."
+sleep 30
+docker exec -i $(docker ps -q -f name=$HOST_SLAVE1) mariadb -uroot -p$SLAVE1_ROOT_PASSWORD < init/01-init.sql
 
-cd ../
+cd ../../
 
 # Resync replication
-echo '*** Resync replication ***'
+echo "${RED}*** Resync replication ***${NC}"
 cd resync
 chmod +x main.sh && ./main.sh
+
 cd ../
 
 # --------------------------
--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Define color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color (reset to default)
+
+# Define the absolute path to the script directory
+BASE_DIR="/var/lib/mysql"
+
+# load env file into the script's environment.
+source $BASE_DIR/env/global.sh
+source $BASE_DIR/env/master.sh
+source $BASE_DIR/env/slave1.sh
+
 # Create network
 docker network create --driver overlay mysql-network
 
@@ -8,56 +20,48 @@ docker stack rm mariadb
 
 # ---------------------------
 
-## Deploy replication
-cd replication
-
 # Deploy master
-echo '*** Deploy container master ***'
-cd master
-chmod +x init.sh && ./init.sh
-cd ../
+echo "${RED}*** Deploy container master ***${NC}"
+mkdir -p $BASE_DIR/replication/master/data
+chmod -R 777 $BASE_DIR/replication/master/data
+chmod +x $BASE_DIR/replication/master/init/init-sql.sh && $BASE_DIR/replication/master/init/init-sql.sh
+docker stack deploy --compose-file $BASE_DIR/replication/master/docker-compose.yaml --detach=false mariadb
+echo "[*] Waiting 30s for master container to be up and running..."
+sleep 30
 
 # Deploy slave1
-echo '*** Deploy container slave1 ***'
-cd slave1
-chmod +x init.sh && ./init.sh
-cd ../
-
-cd ../
+echo "${RED}*** Deploy container slave1 ***${NC}"
+mkdir -p $BASE_DIR/replication/slave1/data
+chmod -R 777 $BASE_DIR/replication/slave1/data
+chmod +x $BASE_DIR/replication/slave1/init/init-sql.sh && $BASE_DIR/replication/slave1/init/init-sql.sh
+docker stack deploy --compose-file $BASE_DIR/replication/slave1/docker-compose.yaml --detach=false mariadb
+echo "[*] Waiting 30s for slave container to be up and running..."
+sleep 30
+docker exec -i $(docker ps -q -f name=$HOST_SLAVE1) mariadb -uroot -p$SLAVE1_ROOT_PASSWORD < init/01-init.sql
 
 # Resync replication
-echo '*** Resync replication ***'
-cd resync
-chmod +x main.sh && ./main.sh
-cd ../
+echo "${RED}*** Resync replication ***${NC}"
+chmod +x $BASE_DIR/resync/main.sh && $BASE_DIR/resync/main.sh
 
 # ---------------------------
 
 echo '*** Deploy services ***'
-cd services
 
 # Deploy MaxScale
-echo '*** Deploy maxscale container ***'
-cd maxscale
-docker stack deploy --compose-file docker-compose.yaml --detach=false mariadb
-cd ../
+echo "${RED}*** Deploy maxscale container ***${NC}"
+docker stack deploy --compose-file $BASE_DIR/services/maxscale/docker-compose.yaml --detach=false mariadb
 
 # Deploy backup
-echo '*** Deploy backup container ***'
-cd backup
-chmod +x init.sh && ./init.sh
-cd ../
+echo "${RED}*** Deploy backup container ***${NC}"
+chmod +x $BASE_DIR/services/backup/init.sh && $BASE_DIR/services/backup/init.sh
 
 # Deploy PMA
-echo '*** Deploy PMA container ***'
-cd pma
-docker stack deploy --compose-file docker-compose.yaml --detach=false mariadb
-cd ../
+echo "${RED}*** Deploy PMA container ***${NC}"
+docker stack deploy --compose-file $BASE_DIR/services/pma/docker-compose.yaml --detach=false mariadb
 
 # Enable startup service
-cd ../
-echo '*** Set auto startup mariadb service ***'
-cp mariadb-repl.service /etc/systemd/system/mariadb-repl.service
+echo "${RED}*** Set auto startup mariadb service ***${NC}"
+cp $BASE_DIR/mariadb-repl.service /etc/systemd/system/mariadb-repl.service
 sudo systemctl enable mariadb-repl.service
 
 # Check status after reboot
