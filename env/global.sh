528fbd516eaff0e7b30907ea17120be74aa6414b
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-10-05 14:04:44 +0700

    update

--- env/global.sh
+++ env/global.sh
@@ -5,10 +5,10 @@
 export TIMEZONE="Asia/jakarta"
 
 export REPL_USERNAME="repl"
-export REPL_PASSWORD="REPL_PASSWORD_SET"
+export REPL_PASSWORD=$(cat /run/secrets/db_repl_paswd)
 
 export MAXSCALE_USERNAME="maxuser"
 export MAXSCALE_PASSWORD="maxpwd"
 
-export SUPER_USERNAME="super_usr"
-export SUPER_PASSWORD="SUPER_PASSWORD_SET"
+export SUPER_USERNAME=$(cat /run/secrets/db_super_user)
+export SUPER_PASSWORD=$(cat /run/secrets/db_super_paswd)
